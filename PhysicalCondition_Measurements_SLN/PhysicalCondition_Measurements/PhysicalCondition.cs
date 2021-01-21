39f47c555525d220bb5915dad6656d780c840b49
Jasmin Bidstrup Nielsen <jasm3956@elevcampus.dk>
2021-01-21 12:38:44 +0100

    Making the CalcPhyCondition().
    Program:
     - Added a new Instance of the Class PhysicalCondition.
     - Displaying the Physical Condition.
    
    PhysicalCondition:
     - Added in Properties and a Constructor.
    - CalcPhyCondition()
               - Made the calculations for Physical Condition, using the User's input from Program.

--- PhysicalCondition_Measurements_SLN/PhysicalCondition_Measurements/PhysicalCondition.cs
+++ PhysicalCondition_Measurements_SLN/PhysicalCondition_Measurements/PhysicalCondition.cs
@@ -6,9 +6,28 @@ namespace PhysicalCondition_Measurements
 {
     class PhysicalCondition
     {
-        public static void CalcPhyCondition()
-        {
+        public decimal Weight { get; set; }
+        public decimal MaxHR { get; set; }
+        public decimal RestHR { get; set; }
 
+        public PhysicalCondition(decimal maxHR, decimal restHR, decimal weight)
+        {
+            Weight = weight;
+            MaxHR = maxHR;
+            RestHR = restHR;
+        }
+
+        /// <summary>
+        /// This is where we calculate the Physical Condition, using the Users input.
+        /// </summary>
+        /// <returns></returns>
+        public decimal CalcPhyCondition()
+        {
+            decimal phyCond = 0;
+
+            phyCond = MaxHR / RestHR * (decimal)15.3;
+
+            return Decimal.Round(phyCond);
         }
 
         public static void CalcO2Consumption()
--- PhysicalCondition_Measurements_SLN/PhysicalCondition_Measurements/Program.cs
+++ PhysicalCondition_Measurements_SLN/PhysicalCondition_Measurements/Program.cs
@@ -10,6 +10,7 @@ namespace PhysicalCondition_Measurements
             decimal restHR = 0;
             decimal maxHR = 0;
 
+            #region User types in Data, Input.
             do
             {
                 Console.WriteLine("Please enter your Weight in KG:");
@@ -25,6 +26,10 @@ namespace PhysicalCondition_Measurements
             {
                 Console.WriteLine("Please enter your Maximum Heart Rate per minute:");
             } while (!decimal.TryParse(Console.ReadLine(), out maxHR));
+            #endregion
+
+            PhysicalCondition physicalCondition = new PhysicalCondition(maxHR, restHR, weight);
+            Console.WriteLine($"Your Physical Condition is: {physicalCondition.CalcPhyCondition()}");
 
             Console.WriteLine("--------------------------------------------------------");
             Console.WriteLine($"Weight: {weight}.\nResting Heart Rate: {restHR}.\nMaximum Heart Rate: {maxHR}");
