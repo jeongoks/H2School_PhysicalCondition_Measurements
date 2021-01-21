d2d0828af8e6fb8d2520b51089b28d3f91db49e4
Jasmin Bidstrup Nielsen <jasm3956@elevcampus.dk>
2021-01-21 11:17:52 +0100

    Added the PhysicalCondition Class.
    PhysicalCondition:
     - Added the Methods with their names but, without any coding.

---
+++ PhysicalCondition_Measurements_SLN/PhysicalCondition_Measurements/PhysicalCondition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhysicalCondition_Measurements
+{
+    class PhysicalCondition
+    {
+        public static void CalcPhyCondition()
+        {
+
+        }
+
+        public static void CalcO2Consumption()
+        {
+
+        }
+    }
+}
